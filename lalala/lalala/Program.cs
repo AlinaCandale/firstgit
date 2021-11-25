3793a71c7d8412771b76cfaac30f01a0961242a2
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-11-25 19:26:55 +0200

    first commit

---
+++ lalala/lalala/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lalala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
